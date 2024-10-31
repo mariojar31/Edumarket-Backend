d281681de84389a404ab32ca4fac2963db81f444
Ana Mogollon <mogollonbuitragoanaelizabeth@gmail>
2024-10-31 12:04:42 -0500

    bd

--- src/main/java/com/edumarket/backend/service/ProductoService.java
+++ src/main/java/com/edumarket/backend/service/ProductoService.java
@@ -1,4 +1,50 @@
 package com.edumarket.backend.service;
 
-public class ProductoService {
+import com.edumarket.backend.model.Estudiante;
+import com.edumarket.backend.model.Producto;
+import com.edumarket.backend.repository.IEstudianteRepository;
+import com.edumarket.backend.repository.IProductoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoService implements IProductoService {
+
+    private IProductoRepository productoRepository;
+
+    @Override
+    public List<Producto> getProductos() {
+        List<Producto> listaProducto = productoRepository.findAll();
+        return listaProducto;
+    }
+
+    @Override
+    public void saveProducto(Producto Producto) {
+        productoRepository.save(Producto);
+
+    }
+
+    @Override
+    public void deleteProducto(Long id) {
+        productoRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Producto findProducto(Long id) {
+        Producto producto= productoRepository.findById(id).orElse(null);
+        return producto;
+    }
+
+    @Override
+    public void editProducto(Long idOriginal, String nuevoNombre, double nuevoPrecio, String nuevaCategoria, int nuevoStock) {
+
+        Producto producto=this.findProducto(idOriginal);
+        producto.setNombreProducto(nuevoNombre);
+        producto.setPrecio(nuevoPrecio);
+        producto.setCategoria(nuevaCategoria);
+        producto.setStock(nuevoStock);
+        this.saveProducto(producto);
+    }
 }
--- src/main/java/com/edumarket/backend/service/UsuarioService.java
+++ src/main/java/com/edumarket/backend/service/UsuarioService.java
@@ -1,4 +1,50 @@
 package com.edumarket.backend.service;
 
-public class UsuarioService {
+import com.edumarket.backend.model.Estudiante;
+import com.edumarket.backend.model.Usuario;
+import com.edumarket.backend.repository.IEstudianteRepository;
+import com.edumarket.backend.repository.IUsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService implements IUsuarioService {
+    private IUsuarioRepository usuarioRepository;
+
+    @Override
+    public List<Usuario> getUsuarios() {
+        List<Usuario> listaUsuario = usuarioRepository.findAll();
+        return listaUsuario;
+    }
+
+    @Override
+    public void saveUsuario(Usuario Usuario) {
+        usuarioRepository.save(Usuario);
+    }
+
+    @Override
+    public void deleteUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+    @Override
+    public Usuario findUsuario(Long id) {
+        Usuario usuario= usuarioRepository.findById(id).orElse(null);
+        return usuario;
+    }
+
+    @Override
+    public void editUsuario(Long idOriginal, String nuevoUsuario, String nuevoEmail, String nuevoContrasena, boolean nuevoIsAdmin, Long nuevoEstudianteid) {
+        Usuario usuario=this.findUsuario(idOriginal);
+        usuario.setNombreUsuario(nuevoUsuario);
+        usuario.setEmail(nuevoEmail);
+        usuario.setContrasena(nuevoContrasena);
+        usuario.setIs_admin(nuevoIsAdmin);
+        usuario.setEstudianteId(nuevoEstudianteid);
+
+        this.saveUsuario(usuario);
+
+
+    }
 }
